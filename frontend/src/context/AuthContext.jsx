eadb5d19d562eee1d01cfffc941023093babb615
samir <samir.warsi2001@gmail.com>
2025-04-13 23:38:54 +0530

    final call

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,38 @@
-import { createContext } from "react";
+import { createContext, useCallback, useEffect, useState } from "react"
+import { BackendUrl } from "../utils/BackendUrl";
+import toast from "react-hot-toast";
 
 const AuthContext = createContext()
 
+export  function AuthProvider({children}) {
+    const [authenticated, setAuthenticated] = useState(false)
+
+    const verifyLogin =useCallback(async() => {
+        try{
+            const response = await fetch(`${BackendUrl}/user/verify`, {
+                method: "GET",
+                credentials: "include",
+              });
+              const result = await response.json();
+              if (result.authenticated === true) {
+                setAuthenticated(true);
+              } else {
+                setAuthenticated(false);
+              }
+        }catch(error){
+            toast.error(error.message)
+        }
+        
+      },[])
+      useEffect(()=>{
+        verifyLogin()
+      },[verifyLogin])
+  return (
+    <div>
+      <AuthContext.Provider value={{authenticated, setAuthenticated}}>
+        {children}
+      </AuthContext.Provider>
+    </div>
+  )
+}
 export default AuthContext
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import {Toaster} from 'react-hot-toast'
-import AuthProvider from "./context/authProvider.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <>
