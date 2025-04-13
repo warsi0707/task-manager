11b0c112002e018d463ab0e60e381ad23c656d80
samir <samir.warsi2001@gmail.com>
2025-04-13 23:15:26 +0530

    final call

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const { MONGO_URL, FRONTEND_URL } = require('./Config/Config');
 const cookieParser = require('cookie-parser');
 const { taskRouter } = require('./routes/task');
 const cors = require('cors')
-
+const path = require('path')
 app.use(cors({
     origin: FRONTEND_URL,
     credentials: true
@@ -15,11 +15,17 @@ app.use(cors({
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"frontend","dist")))
 
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/task",taskRouter)
 
-
+app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'))
+})
 const Main =async()=>{
     app.listen(3000);
     console.log("Server is running on port 3000");
