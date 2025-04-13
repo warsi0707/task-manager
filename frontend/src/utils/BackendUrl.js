82d9996aadfc9cfe06cc642b28f8d4ef06772b05
samir <samir.warsi2001@gmail.com>
2025-04-13 23:42:31 +0530

    final call

--- frontend/src/utils/BackendUrl.js
+++ frontend/src/utils/BackendUrl.js
@@ -1,1 +1,2 @@
-export const BackendUrl = "http://localhost:3000/api/v1"
+// export const BackendUrl = "http://localhost:3000/api/v1"
+export const BackendUrl = "https://task-manager-420p.onrender.com/api/v1"
